dfdded5b5a9677fe3c0e3f383a1909efa9da6e69
Ituking <77528963+Ituking@users.noreply.github.com>
2022-11-16 22:51:19 +0900

    Swift進捗
    Instagram_Cloneの作成

--- Instagram_Clone/View/UploadPost/UploadPostView.swift
+++ Instagram_Clone/View/UploadPost/UploadPostView.swift
@@ -10,27 +10,55 @@ import SwiftUI
 struct UploadPostView: View {
     
     @State private var selectedImage: UIImage?
-    @State var postImage: Image
+    @State var postImage: Image?
     @State var captionText = ""
     @State var ImagePickerRepresented = false
     
     var body: some View {
         
-        Button(action: {
-            self.ImagePickerRepresented.toggle()
-        }, label: {
-            Image(systemName: "plus.circle")
-                .resizable()
-                .scaledToFill()
-                .frame(width: 180, height: 180)
-                .clipped()
-                .padding(.top)
-                .foregroundColor(.black)
-        })
-        .sheet(isPresented: $ImagePickerRepresented) {
-            loadImage()
-        } content: {
-            ImagePicker(image: $selectedImage)
+        if postImage == nil {
+            Button(action: {
+                self.ImagePickerRepresented.toggle()
+            }, label: {
+                Image(systemName: "plus.circle")
+                    .resizable()
+                    .scaledToFill()
+                    .frame(width: 180, height: 180)
+                    .clipped()
+                    .padding(.top)
+                    .foregroundColor(.black)
+            })
+            .sheet(isPresented: $ImagePickerRepresented) {
+                loadImage()
+            } content: {
+                ImagePicker(image: $selectedImage)
+            }
+        } else if let image = postImage {
+            VStack {
+                HStack(alignment: .top) {
+                    image
+                        .resizable()
+                        .scaledToFill()
+                        .frame(width: 96, height: 96)
+                        .clipped()
+                    
+                    TextField("Enter your caption... ", text: $captionText)
+                }
+                
+                Button {
+                    
+                } label: {
+                    Text("Share")
+                        .font(.system(size: 16, weight: .semibold))
+                        .frame(width: 360, height: 50)
+                        .background(Color.blue)
+                        .cornerRadius(5)
+                        .foregroundColor(.white)
+                }
+                
+                Spacer()
+                       
+            }
         }
     }
 }
