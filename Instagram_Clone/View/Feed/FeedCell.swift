243ddbc3fe0b5a3623d986ce8f7033292b63ca10
Ituking <77528963+Ituking@users.noreply.github.com>
2022-11-16 12:10:58 +0900

    Swift進捗
    Instagram_Cloneの作成

---
+++ Instagram_Clone/View/Feed/FeedCell.swift
@@ -0,0 +1,20 @@
+//
+//  FeedCell.swift
+//  Instagram_Clone
+//
+//  Created by 大久保樹 on 2022/11/16.
+//
+
+import SwiftUI
+
+struct FeedCell: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct FeedCell_Previews: PreviewProvider {
+    static var previews: some View {
+        FeedCell()
+    }
+}
