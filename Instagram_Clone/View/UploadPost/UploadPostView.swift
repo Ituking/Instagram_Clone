c649b659536b5deef3c8cfe0b4a2f0d327548aa6
Ituking <77528963+Ituking@users.noreply.github.com>
2022-11-16 22:32:54 +0900

    Swift進捗
     Instagram_Cloneの作成

--- Instagram_Clone/View/UploadPost/UploadPostView.swift
+++ Instagram_Clone/View/UploadPost/UploadPostView.swift
@@ -12,14 +12,34 @@ struct UploadPostView: View {
     @State private var selectedImage: UIImage?
     @State var postImage: Image
     @State var captionText = ""
+    @State var ImagePickerRepresented = false
     
     var body: some View {
-        Image(systemName: "plus.circle")
-            .resizable()
-            .scaledToFill()
-            .frame(width: 180, height: 180)
-            .clipped()
-            .padding(.top)
-            .foregroundColor(.black)
+        
+        Button(action: {
+            self.ImagePickerRepresented.toggle()
+        }, label: {
+            Image(systemName: "plus.circle")
+                .resizable()
+                .scaledToFill()
+                .frame(width: 180, height: 180)
+                .clipped()
+                .padding(.top)
+                .foregroundColor(.black)
+        })
+        .sheet(isPresented: $ImagePickerRepresented) {
+            loadImage()
+        } content: {
+            ImagePicker(image: $selectedImage)
+        }
+    }
+}
+
+extension UploadPostView {
+    func loadImage() {
+        guard let selectedImage = selectedImage else {
+            return
+        }
+        postImage = Image(uiImage: selectedImage)
     }
 }
