d764c7830ed67bab511efdca6f7e1c82553e7b99
Ituking <77528963+Ituking@users.noreply.github.com>
2022-11-16 15:49:28 +0900

    Swift進捗
    Instagram_Cloneの作成

---
+++ Instagram_Clone/View/Search/SearchBar.swift
@@ -0,0 +1,45 @@
+//
+//  SearchBar.swift
+//  Instagram_Clone
+//
+//  Created by 大久保樹 on 2022/11/16.
+//
+
+import SwiftUI
+
+struct SearchBar: View {
+    
+    @Binding var text: String
+    @Binding var isEditing: Bool
+    
+    var body: some View {
+        HStack {
+            TextField("Search...", text: $text)
+                .padding(8)
+                .padding(.horizontal, 24)
+                .background(Color(.systemGray6))
+                .cornerRadius(8)
+                .overlay {
+                    HStack {
+                        Image(systemName: "magnifyingglass")
+                            .foregroundColor(.gray)
+                            .frame(minWidth: 0, maxWidth: .infinity, alignment: .leading)
+                    }
+                }
+            
+            Button(action: {
+                isEditing = false
+                text = ""
+            }, label: {
+                Text("Cancel")
+                    .foregroundColor(.black)
+            })
+            .padding(.trailing, 8)
+            .transition(.move(edge: .trailing))
+            .animation(.default)
+        }
+        .onTapGesture {
+            isEditing = true
+        }
+    }
+}
