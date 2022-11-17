25d0d7e148abbcb9376d8bc1072b6c05697cf8f5
Ituking <77528963+Ituking@users.noreply.github.com>
2022-11-17 22:27:48 +0900

    Swift進捗
    Instagram_Cloneの作成

--- Instagram_Clone/Utils/CustomTextField.swift
+++ Instagram_Clone/Utils/CustomTextField.swift
@@ -20,6 +20,16 @@ struct CustomTextField: View {
                     .foregroundColor(Color(.init(white: 1, alpha: 0.8)))
                     .padding(.leading)
             }
+            
+            HStack {
+                Image(systemName: imageName)
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 20, height: 20)
+                    .foregroundColor(.white)
+                
+                TextField("", text: $text)
+            }
         }
     }
 }
