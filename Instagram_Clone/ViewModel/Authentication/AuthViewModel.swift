07a2341c5c77cd2163c0a43d4592335285eaf8e9
Ituking <77528963+Ituking@users.noreply.github.com>
2022-11-19 00:50:31 +0900

    Swift進捗
    Instagram_Cloneの作成

---
+++ Instagram_Clone/ViewModel/Authentication/AuthViewModel.swift
@@ -0,0 +1,25 @@
+//
+//  AuthViewModel.swift
+//  Instagram_Clone
+//
+//  Created by 大久保樹 on 2022/11/19.
+//
+
+import SwiftUI
+import Firebase
+
+struct AuthViewModel: ObservableObject {
+    
+    func register(withEmail email: String, password: String) {
+        Auth.auth().createUser(withEmail: email, password: password) { (result, err) in
+            if let err = err {
+                print(err.localizedDescription)
+                return
+            }
+            
+            guard let user = result?.user else { return }
+            
+            print(user)
+        }
+    }
+}
