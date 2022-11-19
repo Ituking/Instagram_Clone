d56c724402e7e62f1e31342094a76ee88ec7a3c3
Ituking <77528963+Ituking@users.noreply.github.com>
2022-11-19 14:28:59 +0900

    Swift進捗
    Instagram_Cloneの作成

--- Instagram_Clone/View/Main/MainView.swift
+++ Instagram_Clone/View/Main/MainView.swift
@@ -10,27 +10,29 @@ import SwiftUI
 struct MainView: View {
     
     var body: some View {
-        TabView {
-            FeedView()
-                .tabItem {
-                    Image(systemName: "house")
-                }
-            SearchView()
-                .tabItem {
-                    Image(systemName: "magnifyingglass")
-                }
-            UploadPostView()
-                .tabItem {
-                    Image(systemName: "plus.square")
-                }
-            NotificationsView()
-                .tabItem {
-                    Image(systemName: "heart")
-                }
-            ProfileView()
-                .tabItem {
-                    Image(systemName: "person")
-                }
+        NavigationView {
+            TabView {
+                FeedView()
+                    .tabItem {
+                        Image(systemName: "house")
+                    }
+                SearchView()
+                    .tabItem {
+                        Image(systemName: "magnifyingglass")
+                    }
+                UploadPostView()
+                    .tabItem {
+                        Image(systemName: "plus.square")
+                    }
+                NotificationsView()
+                    .tabItem {
+                        Image(systemName: "heart")
+                    }
+                ProfileView()
+                    .tabItem {
+                        Image(systemName: "person")
+                    }
+            }
         }
     }
 }
