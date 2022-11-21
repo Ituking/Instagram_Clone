83754ab6fb6321025d35224cd9b67e9ebd09ae8f
Ituking <77528963+Ituking@users.noreply.github.com>
2022-11-21 17:43:25 +0900

    Swift進捗
    Instagram_Cloneの作成

--- Instagram_Clone/ViewModel/Profile/ProfileViewModel.swift
+++ Instagram_Clone/ViewModel/Profile/ProfileViewModel.swift
@@ -7,6 +7,7 @@
 
 import SwiftUI
 import Firebase
+import FirebaseStorage
 
 class ProfileViewModel: ObservableObject {
     
@@ -16,15 +17,25 @@ class ProfileViewModel: ObservableObject {
         self.user = user
     }
     
-    func changeProfileImage(image: UIImage) {
+    func changeProfileImage(image: UIImage, completion: @escaping(String) -> Void) {
         guard let imageData = image.jpegData(compressionQuality: 0.5) else { return }
         
-        let ref = Storage.storage().reference(withPath: "/profile_images")
+        let filename = NSUUID().uuidString
+        let ref = Storage.storage().reference(withPath: "/profile_images\(filename)")
         
         ref.putData(imageData, metadata: nil) { (_, err) in
             if let err = err {
                 print(err.localizedDescription)
                 return
+            }
+            
+            ref.downloadURL { (url, err) in
+                if let err = err {
+                    print(err.localizedDescription)
+                    return
+                }
+                
+                guard let imageURL = url?.absoluteString else { return }
             }
         }
     }
