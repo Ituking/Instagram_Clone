0becfaaa9547e92880a88d13e82609a6e7487713
Ituking <77528963+Ituking@users.noreply.github.com>
2022-11-22 13:40:45 +0900

    Swift進捗
    Instagram_Cloneの作成

---
+++ Instagram_Clone/API/ImageUploader.swift
@@ -0,0 +1,38 @@
+//
+//  ImageUploader.swift
+//  Instagram_Clone
+//
+//  Created by 大久保樹 on 2022/11/22.
+//
+
+import SwiftUI
+import Firebase
+import FirebaseStorage
+
+enum uploadType {
+    case profile
+    case post
+}
+
+struct ImageUploader {
+    static func uploadImage(image: UIImage, completion: @escaping(String) -> Void) {
+        guard let imageData = image.jpegData(compressionQuality: 0.5) else { return }
+        
+        let filename = NSUUID().uuidString
+        let ref = Storage.storage().reference(withPath: "/profile_images\(filename)")
+        
+        ref.putData(imageData, metadata: nil) { (_, err) in
+            if let err = err {
+                print(err.localizedDescription)
+                return
+            }
+            
+            ref.downloadURL { (url, err) in
+                if let err = err {
+                    print(err.localizedDescription)
+                    return
+                }
+            }
+        }
+    }
+}
--- Instagram_Clone/ViewModel/Profile/ProfileViewModel.swift
+++ Instagram_Clone/ViewModel/Profile/ProfileViewModel.swift
@@ -18,32 +18,14 @@ class ProfileViewModel: ObservableObject {
     }
     
     func changeProfileImage(image: UIImage, completion: @escaping(String) -> Void) {
-        guard let imageData = image.jpegData(compressionQuality: 0.5) else { return }
         
-        let filename = NSUUID().uuidString
-        let ref = Storage.storage().reference(withPath: "/profile_images\(filename)")
+        guard let uid = user.id else { return }
         
-        ref.putData(imageData, metadata: nil) { (_, err) in
-            if let err = err {
-                print(err.localizedDescription)
-                return
-            }
-            
-            ref.downloadURL { (url, err) in
+        ImageUploader.uploadImage(image: image) { imageURL in
+            Firestore.firestore().collection("users").document(uid).updateData(["profileimageURL": imageURL]) { err in
                 if let err = err {
                     print(err.localizedDescription)
                     return
-                }
-                
-                guard let imageURL = url?.absoluteString else { return }
-                
-                guard let uid = self.user.id else { return }
-                
-                Firestore.firestore().collection("users").document(uid).updateData(["profileimageURL": imageURL]) { err in
-                    if let err = err {
-                        print(err.localizedDescription)
-                        return
-                    }
                 }
             }
         }
